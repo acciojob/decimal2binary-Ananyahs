d6f80edad676699a7d1b7fe95ac9c597ee8eee76
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-25 19:46:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-/*function threeSum(arr, target) {
-// write your code here
-  
-}
-
-module.exports = threeSum;*/
-
 function decimalToBinary(n) {
+  let binary = '';
+  let quotient = n;
+
+  while (quotient !== 0) {
+    const remainder = quotient % 2;
+    binary = remainder + binary;
+    quotient = Math.floor(quotient / 2);
+  }
+
+  return binary;
+}
+    
+  
+ // return parseInt(binaryStr.split("").reverse().join(""))
+ 
+module.exports = decimalToBinary;function decimalToBinary(n) {
   let binary = '';
   let quotient = n;
 

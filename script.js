880efd64eb60eb7af4bf62b8971f3f33bbb15bfd
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 09:40:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-function decimalToBinary(decimal) {
-  if (decimal === 0) {
-    return '0';
-  }
+// function threeSum(arr, target) {
+// write your code here
+  
+// }
 
-  let binary = '';
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
+// module.exports = threeSum;
+
+function decimalToBinary(decimal) {
+  // Use toString() with base 2 to convert decimal to binary
+  const binary = decimal.toString(2);
 
   return binary;
 }
-
-// Example usage
-const decimal = 33;
-const binary = decimalToBinary(decimal);
-console.log(binary);

730cc31e7d5c048e7a28897d148842e11da3fe6e
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 09:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-
-function decimalToBinary(n) {
-  let binary = '';
-  let quotient = n;
-
-  while (quotient !== 0) {
-    const remainder = quotient % 2;
-    binary = remainder + binary;
-    quotient = Math.floor(quotient / 2);
-  }
-
-  return binary;
+function threeSum(arr, target) {
+// write your code here
+  var dec=dec.toString(2)
+	return dec;
 }
-    
-  
- // return parseInt(binaryStr.split("").reverse().join(""))
- 
-module.exports = decimalToBinary;
+
+module.exports = threeSum;

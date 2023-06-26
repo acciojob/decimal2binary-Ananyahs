650ff1c5489a4c711dc640cd0d73a38239cf9288
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 08:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-function decimalToBinary(decimalNumber) {
-  // Base case: if the decimal number is 0, its binary representation is also 0
-  if (decimalNumber === 0) {
-    return '0';
-  }
 
+function decimalToBinary(n) {
   let binary = '';
-  // Convert the decimal number to binary using repeated division by 2
-  while (decimalNumber > 0) {
-    // Get the remainder of the division
-    const remainder = decimalNumber % 2;
-    // Append the remainder to the binary representation
+  let quotient = n;
+
+  while (quotient !== 0) {
+    const remainder = quotient % 2;
     binary = remainder + binary;
-    // Update the decimal number by performing integer division by 2
-    decimalNumber = Math.floor(decimalNumber / 2);
+    quotient = Math.floor(quotient / 2);
   }
 
   return binary;
 }
-
-// Example usage:
-console.log(decimalToBinary(7)); // Output: 111
-console.log(decimalToBinary(10)); // Output: 1010
-console.log(decimalToBinary(33)); // Output: 100001
+    
+  
+ // return parseInt(binaryStr.split("").reverse().join(""))
+ 
+module.exports = decimalToBinary;
